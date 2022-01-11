8c61d87efe2ddbf42dd343171cf90b1b7c4c67e0
unknown <niranjanapdatta1998@gmail.com>
2022-01-11 23:20:35 +0530

    stock page charts

--- src/app/stock-page/stock-page.component.ts
+++ src/app/stock-page/stock-page.component.ts
@@ -22,7 +22,37 @@ export class StockPageComponent implements OnInit {
 
   fy_returns: PotentialReturns = {};
 
-  public barChartOptions: ChartOptions = {
+  graphSelected: number = 0;
+
+  showCloseChart(): void {
+    this.graphSelected = 0;
+  }
+
+  showVolatilityChart(): void {
+    this.graphSelected = 1;
+  }
+
+  showBetaChart(): void {
+    this.graphSelected = 2;
+  }
+
+  showPriceUpDownChart(): void {
+    this.graphSelected = 3;
+  }
+
+  showAvgPriceUpDownChart(): void {
+    this.graphSelected = 4;
+  }
+
+  showReturnsChart(): void {
+    this.graphSelected = 5;
+  }
+
+  public ChartLabels: Label[] = ['Five Years', 'Three Years', 'One Year', 'Six Months'];
+  public ChartLegend = true;
+  public ChartPlugins = [];
+  
+  public closeChartOptions: ChartOptions = {
     responsive: true,
     title: {
       text: 'Close (₹)',
@@ -30,15 +60,69 @@ export class StockPageComponent implements OnInit {
       fontSize: 30
     }
   };
-  public barChartLabels: Label[] = ['Five Years', 'Three Years', 'One Year', 'Six Months'];
-  public barChartType: ChartType = 'line';
-  public barChartLegend = true;
-  public barChartPlugins = [];
+  public closeChartType: ChartType = 'line';
 
-  public barChartData: ChartDataSets[] = [];
-  //   { data: [65, 59, 80, 81, 56, 55, 40], label: 'stock name' },
-  //   { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
-  // ];
+  public closeChartData: ChartDataSets[] = [];
+
+  public volatilityChartOptions: ChartOptions = {
+    responsive: true,
+    title: {
+      text: 'Volatility',
+      display: true,
+      fontSize: 30
+    }
+  };
+  public volatilityChartType: ChartType = 'polarArea';
+
+  public volatilityChartData: ChartDataSets[] = [];
+
+  public betaChartOptions: ChartOptions = {
+    responsive: true,
+    title: {
+      text: 'Beta',
+      display: true,
+      fontSize: 30
+    }
+  };
+  public betaChartType: ChartType = 'radar';
+
+  public betaChartData: ChartDataSets[] = [];
+
+  public priceUpDownChartOptions: ChartOptions = {
+    responsive: true,
+    title: {
+      text: 'Price Up & Down (Days)',
+      display: true,
+      fontSize: 30
+    }
+  };
+  public priceUpDownChartType: ChartType = 'bar';
+
+  public priceUpDownChartData: ChartDataSets[] = [];
+
+  public avgPriceUpDownChartOptions: ChartOptions = {
+    responsive: true,
+    title: {
+      text: 'Average Price Up & Down (₹)',
+      display: true,
+      fontSize: 30
+    }
+  };
+  public avgPriceUpDownChartType: ChartType = 'line';
+
+  public avgPriceUpDownChartData: ChartDataSets[] = [];
+
+  public returnsChartOptions: ChartOptions = {
+    responsive: true,
+    title: {
+      text: 'Potential Returns',
+      display: true,
+      fontSize: 30
+    }
+  };
+  public returnsChartType: ChartType = 'line';
+
+  public returnsChartData: ChartDataSets[] = [];
 
   constructor(
     private stockService: StockService
@@ -72,18 +156,62 @@ export class StockPageComponent implements OnInit {
       5
     );
 
-    if(this.stock.analysis?.[0]?.sm_close != undefined
-      && this.stock.analysis?.[0]?.oy_close != undefined
-      && this.stock.analysis?.[0]?.ty_close != undefined
-      && this.stock.analysis?.[0]?.fy_close != undefined
-      && this.stock.name) {
-      this.barChartData = [
-        { data: [this.stock.analysis?.[0]?.fy_close, 
-          this.stock.analysis?.[0]?.ty_close, 
-          this.stock.analysis?.[0]?.oy_close, 
-          this.stock.analysis?.[0]?.sm_close], label: this.stock.name.toString()}
-      ];
-    }
+    this.closeChartData = [
+      { data: [this.stock.analysis?.[0]?.fy_close, 
+        this.stock.analysis?.[0]?.ty_close, 
+        this.stock.analysis?.[0]?.oy_close, 
+        this.stock.analysis?.[0]?.sm_close], label: this.stock.name?.toString()}
+    ];
+    this.volatilityChartData = [
+      { data: [this.stock.analysis?.[0]?.fy_volatility, 
+        this.stock.analysis?.[0]?.ty_volatility, 
+        this.stock.analysis?.[0]?.oy_volatility, 
+        this.stock.analysis?.[0]?.sm_volatility], label: this.stock.name?.toString()}
+    ];
+    this.betaChartData = [
+      { data: [this.stock.analysis?.[0]?.fy_beta, 
+        this.stock.analysis?.[0]?.ty_beta, 
+        this.stock.analysis?.[0]?.oy_beta, 
+        this.stock.analysis?.[0]?.sm_beta], label: this.stock.name?.toString()}
+    ];
+    this.priceUpDownChartData = [
+      { data: [this.stock.analysis?.[0]?.fy_price_up, 
+        this.stock.analysis?.[0]?.ty_price_up, 
+        this.stock.analysis?.[0]?.oy_price_up, 
+        this.stock.analysis?.[0]?.sm_price_up], label: "Price Up (Days)"},
+      { data: [this.stock.analysis?.[0]?.fy_price_down, 
+          this.stock.analysis?.[0]?.ty_price_down, 
+          this.stock.analysis?.[0]?.oy_price_down, 
+          this.stock.analysis?.[0]?.sm_price_down], label: "Price Down (Days)"}
+    ];
+    this.avgPriceUpDownChartData = [
+      { data: [this.stock.analysis?.[0]?.fy_avg_price_up, 
+        this.stock.analysis?.[0]?.ty_avg_price_up, 
+        this.stock.analysis?.[0]?.oy_avg_price_up, 
+        this.stock.analysis?.[0]?.sm_avg_price_up], label: "Avg. Price Up (₹)"},
+      { data: [this.stock.analysis?.[0]?.fy_avg_price_down, 
+          this.stock.analysis?.[0]?.ty_avg_price_down, 
+          this.stock.analysis?.[0]?.oy_avg_price_down, 
+          this.stock.analysis?.[0]?.sm_avg_price_down], label: "Avg. Price Down (₹)"}
+    ];
+    this.returnsChartData = [
+      { data: [this.fy_returns.profit, 
+        this.ty_returns.profit, 
+        this.oy_returns.profit, 
+        this.sm_returns.profit], label: "Profit (₹)"},
+      { data: [this.fy_returns.dividend, 
+        this.ty_returns.dividend, 
+          this.oy_returns.dividend, 
+          this.sm_returns.dividend], label: "Dividend (₹)"},
+      { data: [this.fy_returns.expenses, 
+        this.ty_returns.expenses, 
+        this.oy_returns.expenses, 
+        this.sm_returns.expenses], label: "Expenses (₹)"},
+      { data: [this.fy_returns.returns, 
+              this.ty_returns.returns, 
+              this.oy_returns.returns, 
+              this.sm_returns.returns], label: "Returns (₹)"}
+    ];
   }
 
   calculateReturns(closeCurrent: number | undefined, close: number | undefined, dividend: number | undefined, expense_ratio: number | undefined, years: number): PotentialReturns {
